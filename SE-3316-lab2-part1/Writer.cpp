72a6456ab81babbd15e72fb51bd8b7959656aef0
Magnus Reeves <71095829+rreeves8@users.noreply.github.com>
2021-11-09 14:28:50 -0500

    Update Writer.cpp

--- SE-3316-lab2-part1/Writer.cpp
+++ SE-3316-lab2-part1/Writer.cpp
@@ -1,60 +1,45 @@
 #include <iostream>
+#include <sstream>
+#include "thread.h"
+#include "SharedObject.h"
+
+using namespace std;
 
 struct SharedMemoryObject{
-	int threadNumber;
-	int 
-
+    int threadID;
+    int reportID;
+    int elapsedTime;
 };
 
-int main(void)
-{
-	std::cout << "I am a Writer" << std::endl;
-	
-	////////////////////////////////////////////////////////////////////////
-	// This is a possible starting point for using threads and shared memory. 
-	// You do not have to start with this
-	////////////////////////////////////////////////////////////////////////
-	/*...
-	Shared<MyShared> shared("sharedMemory", true); //This is the owner of sharedMamory
-	...
-	while(true){
-		...
-		//create thread using user input
-		thread1 = new WriterThread(xyz); //add arguments as needed
-		...
-	}
-	//example for one thread thread1
-	thread1->flag= true;
-	delete thread1;
-	*/
+int main(void){
+    cout << "I am a Writer" << std::endl;
+
+    int threadCount;
+    Shared<SharedMemoryObject> shared("sharedMemory", true); //This is the owner of sharedMamory
+    vector<WriterThread*> threads;
+
+    while(true){
+        char input;
+        cout << "Would you like to create a new thread?" << endl;
+        cin >> input;
+        
+        if(input == 'yes'){
+            int N;
+            cout << "Enter sleep time" << endl;
+            cin >> N;
+
+            WriterThread* newThread = new WriterThread(++threadCount, N);
+
+        }
+        else{
+            for(WriterThread* currentThread : threads){
+                currentThread -> flag = true;
+                delete currentThread;
+            }
+        }   
+    }
+    //example for one thread thread1
+
 }
 
 
-////////////////////////////////////////////////////////////////////////
-// This is a possible starting point for using threads and shared memory. 
-// You do not have to start with this
-////////////////////////////////////////////////////////////////////////
-/*class WriterThread : public Thread{
-	public:
-		int 	threadNum;
-		bool	flag;
-		
-		WriterThread(int in):Thread(8*1000){
-			this->threadNum = in; //or whatever you want/need here
-		}
-
-		virtual long ThreadMain(void) override{
-			...
-			//declare shared memory var so this thread can access it
-			Shared<MyShared> sharedMemory ("sharedMemory");
-			while(true)
-			{
-				//write to shared memory
-				...  
-				if(flag){//Exit loop to end the thread
-					break;
-				}
-			}
-		}
-};
-*/
