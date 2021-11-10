2cab3f155759b51275f35fcf3812b24a0670c772
Magnus Reeves <71095829+rreeves8@users.noreply.github.com>
2021-11-10 15:33:40 -0500

    Update Reader.cpp

--- SE-3313-lab2-part2/Reader.cpp
+++ SE-3313-lab2-part2/Reader.cpp
@@ -3,6 +3,12 @@
 
 using namespace std;
 
+//the semaphore for stopping changes to the SharedMemoryObject
+Semaphore writeSemaphore("writeSemaphore", 1, true);
+	
+//the semaphore for notifying of changes
+Semaphore notifySemaphore("notifySemaphore", 1, true);	
+
 struct SharedMemoryObject{
 	int threadID;
 	int reportID;
@@ -14,14 +20,22 @@ int main(void){
 	
 	//get a refernece to the stored memory
 	Shared<SharedMemoryObject> sharedMemory("sharedMemory");
-
+	
 	while(1){ //run forever until someone kills the terminal
+		
+		//tell other threads to wait the shared memory is being read
+		writeSemaphore.wait();
+		notifySemaphore.wait();
+		
 		cout<< //print the shared memory every second
 			"Thread ID: " << sharedMemory->threadID << " "
 			"Report ID: " << sharedMemory->reportID << " "
 			"Elapsed Time " << sharedMemory->elapsedTime << endl;
 
 		sleep(1);
+		
+		//signal other threads its finished being read
+		writeSemaphore.Signal();
 	}
 
 }
