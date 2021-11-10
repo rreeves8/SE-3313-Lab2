59a98d4463bb3ee182ac3b42b2e2f3e97ce1768d
Magnus Reeves <71095829+rreeves8@users.noreply.github.com>
2021-11-10 12:00:15 -0500

    Comments added

--- SE-3313-lab2-part1/Writer.cpp
+++ SE-3313-lab2-part1/Writer.cpp
@@ -13,22 +13,22 @@ struct SharedMemoryObject{
 
 class WriterThread : public Thread{
 	public:
-		int 	threadNum;
-		int 	threadReported;
-		int 	delay;
+		int 	threadNum; //the thread number
+		int 	threadReported; //the number of times reported
+		int 	delay; //the delay time
 		bool	flag;
 		
-		WriterThread(int threadNum, int delay):Thread(8*1000){
+		WriterThread(int threadNum, int delay):Thread(8*1000){ //constructor for a new thread taking the number and its delay time
 			this->threadNum = threadNum; //or whatever you want/need here
 			this->delay = delay;
 			this->threadReported = 0;
 		}
 
-		virtual long ThreadMain(void) override{
-			
+		virtual long ThreadMain(void) override{ //the thread method to run
 			//declare shared memory var so this thread can access it
 			Shared<SharedMemoryObject> sharedMemory ("sharedMemory");
-			while(true){
+			
+			while(true){ // store values in shared, then sleep, if flag detected kill loop
 				sharedMemory->threadID = this->threadNum;
 				sharedMemory->reportID = threadReported++;
 				sharedMemory->elapsedTime = delay;
@@ -45,24 +45,25 @@ class WriterThread : public Thread{
 int main(void){
 	cout << "I am a Writer" << std::endl;
 
-	int threadCount;
+	int threadCount = 0; //thread counter
 	Shared<SharedMemoryObject> shared("sharedMemory", true); //This is the owner of sharedMamory
-	vector<WriterThread*> threads;
+	vector<WriterThread*> threads; //store a reference to each thread
 
 	while(true){
 		string input;
-		cout << "Would you like to create a new thread?" << endl;
+		cout << "Would you like to create a new thread?" << endl; 
 		cin >> input;
 		
 		if(!input.compare("yes")){
 			int N;
 			cout << "Enter sleep time" << endl;
-			cin >> N;
+			cin >> N; //get delay input
 
-			WriterThread* newThread = new WriterThread(++threadCount, N);
+			WriterThread* newThread = new WriterThread(++threadCount, N); //create new thread and pass new variables
+			threads.push_back(newThread); //store new threas
 
 		}
-		else{
+		else{//if input not yes kill threads
 			for(WriterThread* currentThread : threads){
 				currentThread->flag = true;
 				delete currentThread;
